b1f3cd00ccd8cf91e07e9dfbbe379ae2fc78baf0
Aeotec-ccheng <63321041+Aeotec-ccheng@users.noreply.github.com>
2020-06-30 19:25:49 -0700

    Update to Nano Dimmer (ZW111) parameter settings.
    - Change Parameter 101 to value 8
    - Change Parmaeter 111 to value 1800
    - Add configuration parameter 101
    - Add configuration parameter 102
    - Add configuration parameter 103
    - Add configuration parameter 111
    - Add configuration parameter 112
    - Add configuration parameter 113
    - Add configuration parameter 249
    - Add configuration parameter 255

--- drivers/ZW111-C/device.js
+++ drivers/ZW111-C/device.js
@@ -1,20 +1,20 @@
 'use strict';
 
-const { ZwaveDevice } = require('homey-meshdriver');
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class ZW111C extends ZwaveDevice {
+	
+	onMeshInit() {
+        this.registerCapability('onoff', 'SWITCH_BINARY');
+        this.registerCapability('dim', 'SWITCH_MULTILEVEL');
 
-  onMeshInit() {
-    this.registerCapability('onoff', 'SWITCH_BINARY');
-    this.registerCapability('dim', 'SWITCH_MULTILEVEL');
+        this.registerCapability('measure_power', 'METER');
+        this.registerCapability('meter_power', 'METER');
 
-    this.registerCapability('measure_power', 'METER');
-    this.registerCapability('meter_power', 'METER');
-
-    this.registerReportListener('BASIC', 'BASIC_SET', report => {
-      this.setCapabilityValue('onoff', !!report.Value);
-    });
-  }
+        this.registerReportListener('BASIC', 'BASIC_SET', (report) => {
+          this.setCapabilityValue('onoff', !!report.Value);
+        });
+    }
 
 }
 
