c241d25daad3766752b6216bd2babae7c523ec16
Aeotec-ccheng <63321041+Aeotec-ccheng@users.noreply.github.com>
2020-07-31 01:17:48 -0700

    ZW141 V3 detail update
    Changed from "dim" setting to "windowcovering_set" capability.

--- drivers/ZW141V3/device.js
+++ drivers/ZW141V3/device.js
@@ -2,15 +2,19 @@
 
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
-class ZW141V3 extends ZwaveDevice {
+class ZW141 extends ZwaveDevice {
 	
 	onMeshInit() {
         
-        this.registerCapability('dim', 'SWITCH_MULTILEVEL');
+      //  this.registerCapability('dim', 'SWITCH_MULTILEVEL');
+        this.registerCapability('windowcoverings_set', 'SWITCH_MULTILEVEL');
+//this.registerReportListener('SWITCH_MULTILEVEL', 'SWITCH_MULTILEVEL_REPORT', (report) => {
+ //         this.setCapabilityValue('windowcoverings_set', !!report.Value);
+ //       });
 
        
     }
 
 }
 
-module.exports = ZW141V3;
+module.exports = ZW141;
