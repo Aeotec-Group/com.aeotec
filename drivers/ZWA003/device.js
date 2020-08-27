d9bff2842d3e6b1f27c8a7b0e40a1df84f36ed4f
Aeotec-ccheng <63321041+Aeotec-ccheng@users.noreply.github.com>
2020-08-27 13:05:40 -0700

    ZWA003 - Flow Card Fix
    Resolves issue with Flow Cards using Central Scene.

--- drivers/ZWA003/device.js
+++ drivers/ZWA003/device.js
@@ -1,113 +1,118 @@
 'use strict';
 
-const { ZwaveDevice } = require('homey-meshdriver');
+const Homey = require('homey');
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
-class ZW130 extends ZwaveDevice {
+class ZWA003 extends ZwaveDevice {
 
-  onMeshInit() {
-    // TODO: add battery type (INTERNAL) to driver.compose
-    this._batteryTrigger = this.getDriver().batteryTrigger;
-    this._sceneTrigger = this.getDriver().sceneTrigger;
-    this._dimTrigger = this.getDriver().dimTrigger;
+	onMeshInit() {
+		// TODO: add battery type (INTERNAL) to driver.compose
+        this._batteryTrigger = this.getDriver().batteryTrigger;
+        this._sceneTrigger = this.getDriver().sceneTrigger;
+        this._dimTrigger = this.getDriver().dimTrigger;
 
-    this.registerCapability('measure_battery', 'BATTERY');
+		this.registerCapability('measure_battery', 'BATTERY');
+		this.registerCapability('alarm_battery', 'NOTIFICATION', {
+            reportParser: (report) => {
+                if (report['Notification Type'] === 'Power Management') {
+                    if (report.Event === 13) this._batteryTrigger.trigger(this, null, null);
+                    else if (report.Event === 15) return true;
+                    return false;
+                }
+                return null;
+            },
+        });
 
-    this.registerReportListener('CENTRAL_SCENE', 'CENTRAL_SCENE_NOTIFICATION', report => {
-      if (report['Properties1'] !== undefined
-        && report.Properties1['Key Attributes'] !== undefined
-        && report['Scene Number'] !== undefined) {
-        const data = {
-          button: report['Scene Number'].toString(),
-          scene: report.Properties1['Key Attributes'],
-        };
-        this._sceneTrigger.trigger(this, null, data);
-      }
-    });
-    this.registerReportListener('CONFIGURATION', 'CONFIGURATION_REPORT', report => {
-      if (report['Parameter Number'] !== undefined
-        && report['Configuration Value'] !== undefined) {
-        if (report['Parameter Number'] === 9) {
-          const data = {
-            button: report['Configuration Value'][0].toString(),
-            scene: (report['Configuration Value'][1] === 1) ? 'Key Slide Up' : 'Key Slide Down',
-          };
-          this._sceneTrigger.trigger(this, null, data);
+		this.registerReportListener('CENTRAL_SCENE', 'CENTRAL_SCENE_NOTIFICATION', (report) => {
+			if (report.hasOwnProperty('Properties1') &&
+				report.Properties1.hasOwnProperty('Key Attributes') &&
+				report.hasOwnProperty('Scene Number')) {
+				const data = {
+					button: report['Scene Number'].toString(),
+					scene: report.Properties1['Key Attributes'],
+				};
+				this._sceneTrigger.trigger(this, null, data);
+			}
+		});
+		this.registerReportListener('CONFIGURATION', 'CONFIGURATION_REPORT', (report) => {
+			if (report.hasOwnProperty('Parameter Number') &&
+				report.hasOwnProperty('Configuration Value')) {
+				if (report['Parameter Number'] === 9) {
+					const data = {
+						button: report['Configuration Value'][0].toString(),
+						scene: (report['Configuration Value'][1] === 1) ? 'Key Slide Up' : 'Key Slide Down',
+					};
+					this._sceneTrigger.trigger(this, null, data);
+				}
+                	if (report['Parameter Number'] === 10) {
+					let value = Math.round(report['Configuration Value'][2] / 2) / 100;
+					if (value < 0.5) value = Math.max(value - 0.05, 0);
+					const token = {
+						dim: value,
+					};
+					const data = {
+						button: report['Configuration Value'][0].toString(),
+					};
+					this._dimTrigger.trigger(this, token, data);
+				}
+			}
+		});
+	}
+
+	async onSettings(oldSettings, newSettings, changedKeys) {
+		super.onSettings(oldSettings, newSettings, changedKeys);
+
+		if (changedKeys.includes('rgb_name')
+			|| changedKeys.includes('rgb_r')
+			|| changedKeys.includes('rgb_g')
+			|| changedKeys.includes('rgb_b')) {
+            this.log('color changed');
+
+            if (newSettings.rgb_name === 'custom'
+                && newSettings.hasOwnProperty('rgb_r')
+                && newSettings.hasOwnProperty('rgb_g')
+                && newSettings.hasOwnProperty('rgb_b')) {
+            	this.log('custom color');
+                return await this.configurationSet({
+                    index: 5,
+                    size: 4,
+                }, new Buffer([newSettings.rgb_r, newSettings.rgb_g, newSettings.rgb_b, 0]));
+            }
+            this.log('listed color');
+
+            const valueArray = newSettings.rgb_name.split(',');
+            const multiplier = newSettings.rgb_name_level / 100 || 1;
+
+            return await this.configurationSet({
+                index: 5,
+                size: 4,
+            }, new Buffer([Math.round(valueArray[0] * multiplier), Math.round(valueArray[1] * multiplier), Math.round(valueArray[2] * multiplier), 0]));
         }
-        if (report['Parameter Number'] === 10) {
-          let value = Math.round(report['Configuration Value'][2] / 2) / 100;
-          if (value < 0.5) value = Math.max(value - 0.05, 0);
-          const token = {
-            dim: value,
-          };
-          const data = {
-            button: report['Configuration Value'][0].toString(),
-          };
-          this._dimTrigger.trigger(this, token, data);
-        }
-      }
-    });
-  }
 
-  // eslint-disable-next-line consistent-return
-  async onSettings(oldSettings, newSettings, changedKeys) {
-    await super.onSettings(oldSettings, newSettings, changedKeys);
-
-    if (changedKeys.includes('rgb_name')
-      || changedKeys.includes('rgb_r')
-      || changedKeys.includes('rgb_g')
-      || changedKeys.includes('rgb_b')) {
-      this.log('color changed');
-
-      if (newSettings.rgb_name === 'custom'
-        && newSettings['rgb_r'] !== undefined
-        && newSettings['rgb_g'] !== undefined
-        && newSettings['rgb_b'] !== undefined) {
-        this.log('custom color');
-        return this.configurationSet({
-          index: 5,
-          size: 4,
-        }, Buffer.from([newSettings.rgb_r, newSettings.rgb_g, newSettings.rgb_b, 0]));
-      }
-      this.log('listed color');
-
-      const valueArray = newSettings.rgb_name.split(',');
-      const multiplier = newSettings.rgb_name_level / 100 || 1;
-
-      return this.configurationSet({
-        index: 5,
-        size: 4,
-      }, Buffer.from([
-        Math.round(valueArray[0] * multiplier),
-        Math.round(valueArray[1] * multiplier),
-        Math.round(valueArray[2] * multiplier), 0]));
+        this.log(changedKeys);
     }
 
-    this.log(changedKeys);
-  }
+	sceneRunListener(args, state) {
+		if (!args) return Promise.reject('No arguments provided');
+		if (!state) return Promise.reject('No state provided');
 
-  sceneRunListener(args, state) {
-    if (!args) return Promise.reject(new Error('No arguments provided'));
-    if (!state) return Promise.reject(new Error('No state provided'));
+		if (args.hasOwnProperty('button') &&
+			state.hasOwnProperty('button') &&
+			args.hasOwnProperty('scene') &&
+			state.hasOwnProperty('scene')) {
+        		return (args.button === state.button && args.scene === state.scene);
+		} return Promise.reject('Button or scene undefined in args or state');
+	}
 
-    if (args.button !== undefined
-      && state.button !== undefined
-      && args.scene !== undefined
-      && state.scene !== undefined) {
-      return (args.button === state.button && args.scene === state.scene);
-    }
-    return Promise.reject(new Error('Button or scene undefined in args or state'));
-  }
+	dimRunListener(args, state) {
+		if (!args) return Promise.reject('No arguments provided');
+		if (!state) return Promise.reject('No state provided');
 
-  dimRunListener(args, state) {
-    if (!args) return Promise.reject(new Error('No arguments provided'));
-    if (!state) return Promise.reject(new Error('No state provided'));
-
-    if (args.button !== undefined && state.button !== undefined) {
-      return (args.button === state.button);
-    }
-    return Promise.reject(new Error('Button undefined in args or state'));
-  }
-
+		if (args.hasOwnProperty('button') &&
+			state.hasOwnProperty('button')) {
+        		return (args.button === state.button);
+		} return Promise.reject('Button undefined in args or state');
+	}
 }
 
-module.exports = ZW130;
+module.exports = ZWA003;
