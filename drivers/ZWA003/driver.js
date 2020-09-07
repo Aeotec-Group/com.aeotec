e3c54eb514a6267e74748907fa3ed1e5c0f1a4a9
Aeotec-ccheng <63321041+Aeotec-ccheng@users.noreply.github.com>
2020-09-07 14:49:16 -0700

    ZWA003 NanoMote Quad update.
    27.8.20
    
    - fixed Id problem in scenes.
    
    01.09.20
    
    - cleared code

--- drivers/ZWA003/driver.js
+++ drivers/ZWA003/driver.js
@@ -2,20 +2,15 @@
 
 const Homey = require('homey');
 
-class ZW130Driver extends Homey.Driver {
+class ZWA003Driver extends Homey.Driver {
+    onInit() {
+        super.onInit();
 
-  onInit() {
-    super.onInit();
-
-    this.batteryTrigger = new Homey.FlowCardTriggerDevice('zw130_battery_full').register();
-    this.sceneTrigger = new Homey.FlowCardTriggerDevice('zw130_scene').register().registerRunListener((args, state) => {
-      return args.device.sceneRunListener(args, state);
-    });
-    this.dimTrigger = new Homey.FlowCardTriggerDevice('zw130_dim').register().registerRunListener((args, state) => {
-      return args.device.dimRunListener(args, state);
-    });
-  }
-
+        this.batteryTrigger = new Homey.FlowCardTriggerDevice('ZWA003_battery_full').register();
+        this.sceneTrigger = new Homey.FlowCardTriggerDevice('ZWA003_scene').register().registerRunListener((args, state) => {
+            return args.device.sceneRunListener(args, state);
+        });
+            }
 }
 
-module.exports = ZW130Driver;
+module.exports = ZWA003Driver;
